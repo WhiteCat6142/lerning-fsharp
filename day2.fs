93a2bd33929f2acd2dfa7db1a0cbbf04bd112903
WhiteCat6142 <WhiteCat6142@users.noreply.github.com>
2015-12-30 15:48:35 +0900

    Update day2.fs

--- day2.fs
+++ day2.fs
@@ -19,3 +19,11 @@ seq { for i in 2 .. 1000 -> i}
 let sf ( f: 'T -> 'T list -> bool) a b = Seq.fold (fun x y ->if(f y x) then y::x else x) a b
 let check2 x l = not(List.exists (fun i -> x%i=0) l)
 seq{3..2..10000}|>sf check2 [2;]|>List.rev|>printseq;;
+
+(*素数v3*)
+let count n = Seq.initInfinite (fun k -> k + n)
+let check l x = not(List.exists (fun i -> x%i=0) l)
+let prime (s:'int list) =
+ let a=count(s.Head)|>Seq.find (check s)
+ Some(a,a::s)
+Seq.unfold prime [2;]|>printseq;;
